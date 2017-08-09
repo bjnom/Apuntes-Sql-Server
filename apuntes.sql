7b0c94dcb7e816d4dd0e1f624677de3844b154eb
bjnom <bjn.olguin@gmail.com>
2017-08-09 04:18:05 -0400

    Create apuntes.sql

---
+++ apuntes.sql
@@ -0,0 +1,22 @@
+/*
+Listar Descripcion de columnas
+*/
+select 
+    st.name [Table],
+    sc.name [Column],
+    sep.value [Description]
+from sys.tables st
+inner join sys.columns sc on st.object_id = sc.object_id
+left join sys.extended_properties sep on st.object_id = sep.major_id
+                                     and sc.column_id = sep.minor_id
+                                     and sep.name = 'MS_Description'
+where st.name = 'NombreTabla'
+
+/*
+Describir Tabla
+*/
+sp_help NombreTabla
+
+/*
+Crear Proceso Almacenado
+*/
